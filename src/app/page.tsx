b2c40b688bbf6792c753af583ca73fe0b3736c8d
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-07-26 08:35:22 +0000

    starting the about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import About from "../components/About";
+
 export default function Home() {
   return (
+    <>
+    
     <h2 className="text-xl font-bold underline text-sky-700 text-center m-10 mt-20">
       Hello world!
     </h2>
+    <About/>
+    </>
+    
   )
 }
